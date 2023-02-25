b1d7e6a760a437b7db00267303a10dbea1bbf9f6
EthiemGuerrero <ethiemalex03@gmail.com>
2023-02-25 14:30:16 -0500

    acomodar peticion

--- views/Chats.js
+++ views/Chats.js
@@ -1,9 +1,10 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
-import { useEffect } from 'react/cjs/react.production.min'
+import { useEffect, useState } from 'react/cjs/react.production.min'
 import axios from 'axios'
 
 export default function Chats({navigation}) {
+	const [obj, setObj] = useState('holas')
 	const login = () => {
 		axios.get('https://api.spaceflightnewsapi.net/v3/articles?_limit=20')
 			.then(res => console.log(res))
@@ -13,6 +14,7 @@ export default function Chats({navigation}) {
       <Text>Chats</Text>
       <Button title='Home' onPress={()=>navigation.navigate('Home')}/>
 			<Button title='login' onPress={login}/>
+			<Text>{obj}</Text>
     </View>
   )
 }
